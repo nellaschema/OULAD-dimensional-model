170fc87aab34a0c374edea31631757b568aa0911
nicole.salazar@ftwfoundation.org <nicole.salazar@ftwfoundation.org>
2026-09-10 10:04:29 +0000

    updated dim_demog to align with the suggestions

--- src/mart/dim_demographics.sql
+++ src/mart/dim_demographics.sql
@@ -1,16 +1,25 @@
-CREATE OR REPLACE TABLE `ftw-week-07`.`03-mart`.dim_demographics
+CREATE OR REPLACE TABLE ftw-week-07.03-mart.dim_demographics
 USING DELTA
 AS
-SELECT
-  CAST(row_number() OVER (ORDER BY gender, region, highest_education, imd_band, age_band, disability) AS STRING) AS demographics_key,
-  gender,
-  region,
-  highest_education,
-  imd_band,
-  age_band,
-  disability,
-  final_result
-FROM (
-  SELECT DISTINCT gender, region, highest_education, imd_band, age_band, disability, final_result
-  FROM `ftw-week-07`.`02-clean`.student_info_clean
-);
+SELECT DISTINCT
+SHA2(
+CONCAT_WS(
+'||',
+COALESCE(gender, 'UNKNOWN'),
+COALESCE(region, 'UNKNOWN'),
+COALESCE(highest_education, 'UNKNOWN'),
+COALESCE(imd_band, 'UNKNOWN'),
+COALESCE(age_band, 'UNKNOWN'),
+COALESCE(disability, 'UNKNOWN'),
+COALESCE(final_result, 'UNKNOWN')
+),
+256
+) AS demographics_key,
+gender,
+region,
+highest_education,
+imd_band,
+age_band,
+disability,
+final_result
+FROM ftw-week-07.02-clean.student_info_clean;
