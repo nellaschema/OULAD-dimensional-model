edca896bc9394e2a0b94259d9108614c03f54828
nicole.salazar@ftwfoundation.org <nicole.salazar@ftwfoundation.org>
2026-09-10 02:45:04 +0000

    dim_course

---
+++ src/mart/dim_course.sql
@@ -0,0 +1,11 @@
+-----  dim_course
+CREATE OR REPLACE TABLE `ftw-week-07`.`03-mart`.dim_course
+USING DELTA
+AS
+SELECT
+  code_module AS course_key,
+  code_module
+FROM (
+  SELECT DISTINCT code_module
+  FROM `ftw-week-07`.`02-clean`.courses_clean
+);
